a10393d967d2a417fb0553280ad5e5f6dc48e4e3
Ben Howl <mrbenhowl@gmail.com>
2023-07-19 12:39:07 +1000

    chore: add SSR page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,18 @@ import '@/styles/globalStyle.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
- 
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
- 
+
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
- 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+export default function App ({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
- 
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page)
+
   return getLayout(<Component {...pageProps} />)
 }
--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPageWithLayout } from '../_app'
+import { GetServerSideProps } from 'next'
 import { ReactElement } from 'react'
 import Head from 'next/head'
 import Layout from '@/components/layout'
 
-const Transactions: NextPageWithLayout = () => {
+type Props = {
+  transactions: Transaction[]
+}
+
+interface Transaction {
+  description: string
+  amount: number
+}
+
+const Transactions: NextPageWithLayout<Props> = ({ transactions }: Props) => {
   return (
     <>
       <Head>
@@ -11,6 +21,15 @@ const Transactions: NextPageWithLayout = () => {
       </Head>
       <main>
         <div>Transactions</div>
+        <div>
+          {transactions.map((transaction, key: any) => {
+            return (
+              <div key={key}>
+                {transaction.description}, {transaction.amount}
+              </div>
+            )
+          })}
+        </div>
       </main>
     </>
   )
@@ -21,3 +40,13 @@ Transactions.getLayout = function getLayout (page: ReactElement) {
 }
 
 export default Transactions
+
+export async function getServerSideProps (): Promise<{
+  props: { transactions: Transaction[] }
+}> {
+  const res = await fetch(`http://localhost:4545/transactions`)
+  const transactions = await res.json()
+
+  // Pass data to the page via props
+  return { props: { transactions } }
+}
